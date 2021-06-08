10bd66e84281223935e7660f03d63e09b4f35801
图兔兔 <1479761523@qq.com>
2021-06-08 17:11:44 +0800

    添加新方法

--- 我的项目的一般配置模块1.0/my_lib/func.py
+++ 我的项目的一般配置模块1.0/my_lib/func.py
@@ -7,6 +7,7 @@ import shutil
 import difflib
 import tempfile
 import subprocess
+import colorsys
 
 import requests
 from bs4 import BeautifulSoup
@@ -610,6 +611,47 @@ def randomcolor():
     for _ in range(6):
         color += color_arr[random.randint(0, 14)]
     return "#" + color
+
+
+def get_n_hls_colors(num):
+    """生成多个hls颜色"""
+    hls_colors = []
+    i = 0
+    step = 360.0 / num
+    while i < 360:
+        h = i
+        s = 90 + random.random() * 10
+        l = 50 + random.random() * 10
+        _hlsc = [h / 360.0, l / 100.0, s / 100.0]
+        hls_colors.append(_hlsc)
+        i += step
+    return hls_colors
+
+
+def random_colors(num):
+    """生成多个差别较大的颜色"""
+    rgb_colors = []
+    if num < 1:
+        return rgb_colors
+    hls_colors = get_n_hls_colors(num)
+    for hlsc in hls_colors:
+        _r, _g, _b = colorsys.hls_to_rgb(hlsc[0], hlsc[1], hlsc[2])
+        r, g, b = [int(x * 255.0) for x in (_r, _g, _b)]
+        rgb_colors.append([r, g, b])
+
+    return rgb_colors
+
+
+def random_colors_str(num):
+    colors = []
+    for color in random_colors(num):
+        str_color = "#"
+        for i in color:
+            str_hex_i = str(hex(i))[2:]
+            str_hex_i = "0" * (2-len(str_hex_i)) + str_hex_i
+            str_color += str_hex_i
+        colors.append(str_color)
+    return colors
 
 
 def chinese_num():
